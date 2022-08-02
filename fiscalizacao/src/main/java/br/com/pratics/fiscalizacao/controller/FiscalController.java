37ecf42df189587e60dd8a0fc1d380730a0dd2c4
Samuel Lopes <samek.vitor1@gmail.com>
2022-08-01 22:39:31 -0300

    start

--- fiscalizacao/src/main/java/br/com/pratics/fiscalizacao/controller/FiscalController.java
+++ fiscalizacao/src/main/java/br/com/pratics/fiscalizacao/controller/FiscalController.java
@@ -1,5 +1,19 @@
 package br.com.pratics.fiscalizacao.controller;
 
-public class FiscalController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/fiscal")
+public class FiscalController {
+	
+	
+	@GetMapping("/teste")
+	public String teste() {
+		
+		return "Banana";
+	}
+	
+	
 }
